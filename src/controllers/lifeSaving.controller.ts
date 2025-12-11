08e1a98ca497db0a6617b52303be59be5c26f7fe
Masaniprakash <Kanagapandi07@gmail.com>
2025-12-11 22:28:32 +0530

    when customer form add customer table

--- src/controllers/lifeSaving.controller.ts
+++ src/controllers/lifeSaving.controller.ts
@@ -2,12 +2,20 @@
 import { Request, Response } from "express";
 import LifeSaving from "../models/lifeSaving.model";
 import lifeSavingModel from "../models/lifeSaving.model";
-import { ReE, ReS, toAwait } from "../services/util.service";
+import { isNull, ReE, ReS, toAwait } from "../services/util.service";
 import httpStatus from "http-status";
 import { toLowerCaseObj } from "./common";
+import { Customer } from "../models/customer.model";
 
 export const createLifeSaving = async (req: Request, res: Response) => {
     let payload = toLowerCaseObj(req.body),err;
+
+    let fields = [ "mobileNo", "email", "nameOfCustomer" ];
+    let inVaildFields = fields.filter(x => isNull(payload[x]));
+    if (inVaildFields.length > 0) {
+        return ReE(res, { message: `Please enter required fields ${inVaildFields}!.` }, httpStatus.BAD_REQUEST);
+    }
+    let { mobileNo, email, pincode, communicationAddress,nameOfCustomer} = payload
     
     let checkAlready;
     [err, checkAlready] = await toAwait(lifeSavingModel.findOne(payload));
@@ -19,6 +27,26 @@ export const createLifeSaving = async (req: Request, res: Response) => {
     let cretaeLSS;
     [err,cretaeLSS] = await toAwait(lifeSavingModel.create(payload));    
     if (err) return ReE(res, err, httpStatus.INTERNAL_SERVER_ERROR);
+
+    let checkCustomer;
+    [err,checkCustomer] = await toAwait(Customer.findOne({email, phone:mobileNo}))
+
+    if (err) return ReE(res, err, httpStatus.INTERNAL_SERVER_ERROR);
+    if (!checkCustomer) {
+        let customer;
+        [err, customer] = await toAwait(Customer.create({
+            phone:mobileNo,
+            email,
+            name : nameOfCustomer,
+            pincode, 
+            address:communicationAddress
+        }));
+        if (err) return ReE(res, err, httpStatus.INTERNAL_SERVER_ERROR);
+        if (!customer) {
+            return ReE(res, { message: `Failed to create customer!.` }, httpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
+
     ReS(res, { message: "LifeSaving created"}, httpStatus.OK);
 };
 
--- src/controllers/plotBookingForm.controller.ts
+++ src/controllers/plotBookingForm.controller.ts
@@ -1,16 +1,30 @@
 import httpStatus from "http-status"
-import { ReE, ReS, toAwait } from "../services/util.service"
+import { isNull, isPhone, ReE, ReS, toAwait } from "../services/util.service"
 import { Request, Response } from "express"
 import plotBookingFormModel from "../models/plotBookingForm.model";
 import { IPlotBookingForm } from "../type/plotBookingForm";
 import { toLowerCaseObj } from "./common";
-import { PutObjectCommand } from "@aws-sdk/client-s3";
-import { s3 } from "../services/digitalOceanConfig";
+import { Customer } from "../models/customer.model";
 
 export const createPlotBookingForm = async (req: Request, res: Response) => {
     let err;
+    let photo;
+    if(req.body.photo){
+        photo = req.body.photo
+        delete req.body.photo
+    }
     let body = toLowerCaseObj(req.body);
 
+    
+    let fields = [ "mobileNo", "email", "nameOfCustomer" ];
+    let inVaildFields = fields.filter(x => isNull(body[x]));
+    if (inVaildFields.length > 0) {
+        return ReE(res, { message: `Please enter required fields ${inVaildFields}!.` }, httpStatus.BAD_REQUEST);
+    }
+    let { mobileNo, email, pincode, address,nameOfCustomer} = body
+    if (!isPhone(mobileNo)) {
+        return ReE(res, { message: `Invalid mobile number!.` }, httpStatus.BAD_REQUEST)
+    }
     // 🔍 check for duplicate with all provided fields
     let checkAlready;
     [err, checkAlready] = await toAwait(plotBookingFormModel.findOne(body));
@@ -18,36 +32,10 @@ export const createPlotBookingForm = async (req: Request, res: Response) => {
     if (checkAlready) {
         return ReE(res, { message: `This plotBookingForm already exists.` }, httpStatus.BAD_REQUEST);
     }
-
-    if (req.file) {
-        try {
-            const BUCKET = process.env.DO_SPACES_BUCKET;
-            const CDN_URL = process.env.DO_SPACES_CDN;
-            if (!BUCKET || !CDN_URL) {
-                return ReE(res, { message: "Missing environment variables for Digital Ocean" }, httpStatus.INTERNAL_SERVER_ERROR);
-            }
-            const files = req.file as Express.Multer.File;
-            if (!files) {
-                return ReE(res, { message: "please upload at least one files" }, httpStatus.BAD_REQUEST);
-            }
-            const urls: string[] = [];
-            const file = files;
-            const fileName = `uploads/${Date.now()}_${file.originalname?.replace(/\s+/g, '')}`;
-            await s3.send(
-                new PutObjectCommand({
-                    Bucket: BUCKET,
-                    Key: fileName,
-                    Body: file.buffer,
-                    ACL: "public-read",
-                    ContentType: file.mimetype,
-                }));
-            let url = `${CDN_URL}/${fileName}`
-            body = { ...body, photo: url };
-        } catch (error) {
-            return ReE(res, error, httpStatus.INTERNAL_SERVER_ERROR);
-        }
+    email = email.trim().toLowerCase();
+    if(photo){
+        body.photo = photo;
     }
-
     let plotBookingForm;
     [err, plotBookingForm] = await toAwait(plotBookingFormModel.create(body));
     if (err) return ReE(res, err, httpStatus.INTERNAL_SERVER_ERROR);
@@ -55,7 +43,27 @@ export const createPlotBookingForm = async (req: Request, res: Response) => {
         return ReE(res, { message: `Failed to create plotBookingForm!` }, httpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    return ReS(res, { message: "plotBookingForm created successfully", data: plotBookingForm }, httpStatus.OK);
+    let checkCustomer;
+    [err,checkCustomer] = await toAwait(Customer.findOne({email, phone:mobileNo}))
+
+    if (err) return ReE(res, err, httpStatus.INTERNAL_SERVER_ERROR);
+    
+    if (!checkCustomer) {
+        let customer;
+        [err, customer] = await toAwait(Customer.create({
+            phone:mobileNo,
+            email,
+            name : nameOfCustomer,
+            pincode, 
+            address
+        }));
+        if (err) return ReE(res, err, httpStatus.INTERNAL_SERVER_ERROR);
+        if (!customer) {
+            return ReE(res, { message: `Failed to create customer!.` }, httpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
+
+    return ReS(res, { message: "plotBookingForm created successfully" }, httpStatus.OK);
 };
 
 
