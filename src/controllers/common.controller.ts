ec551573e61a37f947a2d1b620d5d36fca208182
AvvaiAravind <avvaiaravind@gmail.com>
2026-01-21 22:59:29 +0530

    modifeid at customer controller

--- src/controllers/common.controller.ts
+++ src/controllers/common.controller.ts
@@ -344,15 +344,15 @@ export const UpdateCommonData = async (req: CustomRequest, res: Response) => {
       );
     }
 
-    if (!general.editDeleteReason) {
-      return ReE(
-        res,
-        {
-          message: `If update general then general.editDeleteReason is required`,
-        },
-        httpStatus.BAD_REQUEST
-      );
-    }
+    // if (!general.editDeleteReason) {
+    //   return ReE(
+    //     res,
+    //     {
+    //       message: `If update general then general.editDeleteReason is required`,
+    //     },
+    //     httpStatus.BAD_REQUEST
+    //   );
+    // }
 
     if (general.status) {
       general.status = general.status.toLowerCase();
