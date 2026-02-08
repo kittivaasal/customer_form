0a6263ac62426ff9a523e84e453e77ce0eb021b4
Masaniprakash <masaniprakash05@gmail.com>
2026-02-08 18:01:42 +0530

    update logic getbyBill id added downline avail

--- src/controllers/common.controller.ts
+++ src/controllers/common.controller.ts
@@ -1330,13 +1330,40 @@ export const getByIdBilling = async (req: Request, res: Response) => {
       httpStatus.NOT_FOUND
     );
   }
-  getBilling = getBilling as any
+
+  getBilling = (getBilling as any).toObject();
+
   let getAllFull, generalId, customerId;
   if (isValidObjectId(getBilling.general)) {
     generalId = getBilling.general
   } else if (isValidObjectId(getBilling.general._id)) {
     generalId = getBilling.general._id
   }
+
+  let checkDownLineforCed;
+  let cedId = ""
+  if(isValidObjectId(getBilling.customer.cedId)){
+    cedId = getBilling.customer.cedId
+  }else if(isValidObjectId(getBilling.customer.cedId._id)){
+    cedId = getBilling.customer.cedId._id
+  }
+  [err, checkDownLineforCed] = await toAwait(
+    MarketDetail.findOne({ headBy : cedId })
+  )
+
+  if(err){
+    return ReE(res, err, httpStatus.INTERNAL_SERVER_ERROR);
+  }
+  console.log(checkDownLineforCed)
+  if(!checkDownLineforCed){
+    getBilling.customer.cedId.downLine = false
+    // console.log("downline not found", getBilling.customer.cedId)
+  }else{
+    getBilling.customer.cedId.downLine = true
+    // console.log("downline not found", getBilling.customer.cedId)
+  }
+
+
 
   if (isValidObjectId(getBilling.customer)) {
     customerId = getBilling.customer
